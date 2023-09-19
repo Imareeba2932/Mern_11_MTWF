964593161f4e656681bd559124e2d12106b82017
Areeba Shakeel <shakeelareeba02>
2023-09-19 12:43:48 +0530

    js

---
+++ basics.js
@@ -0,0 +1,24 @@
+// Types of Variables
+// 1. var
+// 2. let
+// 3. const
+
+console.log("Areeba")
+console.log("Shakeel")
+
+let age=25;
+console.log(age)
+
+age=45;
+console.log("Now I'm",age,"years old.")
+
+// const dob=45;
+// console.log(dob)
+
+// dob=35;
+// console.log(dob)
+
+let firstName="Rahul"
+let lastName="Singh"
+let fullName = firstName + " " + lastName
+console.log("Full Name :", fullName)
