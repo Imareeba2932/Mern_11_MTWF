c8185616baefd74c894081acccc71305becccaca
Areeba Shakeel <shakeelareeba02>
2023-11-03 12:16:31 +0530

    models

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,14 @@ const port = 5000;
 app.get('/',(req, res) => {
     res.send('Server is successfully running')
 })
+app.get('/about',(req, res) => {
+    res.send('This is about page')
+})
+app.get('/contact',(req, res) => {
+    res.send('This is contact page')
+})
+
+const mongoose = require('./connection');
 
 app.listen(port, () => {
     console.log('Server is running on port: 5000');
