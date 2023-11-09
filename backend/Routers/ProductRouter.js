3d5d2800bfe329f00b95fac3a9ccb1f339acca03
Areeba Shakeel <shakeelareeba02>
2023-11-09 18:18:52 +0530

    CRUD

--- backend/Routers/ProductRouter.js
+++ backend/Routers/ProductRouter.js
@@ -5,7 +5,8 @@ const Model = require('../Models/ProductModel');
 router.post('/add', (req,res) => {
     console.log(req.body)
     // Storing data to mongoDB
-    new Model(req.body).save()
+    // to add the data in database
+    new Model(req.body).save()  
     .then((result) => {
         res.json(result)
     }).catch((err) => {
@@ -13,4 +14,26 @@ router.post('/add', (req,res) => {
         res.status(500).json(err)
     });
 });
+
+router.get('/getall',(req,res) => {
+    // empty brackets will give all the data from the database
+    Model.find({})
+    .then((result) => {
+        res.json(result)
+    }).catch((err) => {
+        console.error(err)
+        res.status(500).json(err)
+    });
+    
+});
+router.get('/getbyid/:id',(req,res) => {
+    Model.findById(req.params.id)
+    .then((result) => {
+        res.json(result)
+    }).catch((err) => {
+        console.error(err)
+        res.status(500).json(err)
+    });
+})
+
 module.exports = router;
